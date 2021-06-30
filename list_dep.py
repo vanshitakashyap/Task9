0b61d2a57b6b4371097b895c84ae3202f2ef37ad
vanshitakashyap <85801574+vanshitakashyap@users.noreply.github.com>
2021-06-30 19:37:00 +0530

    Create list_dep.py

---
+++ list_dep.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+
+print("content-type: text/html")
+print()
+
+
+
+
+output = subprocess.getoutput(" sudo kubectl get deployment --kubeconfig /root/k8s_tasks/admin.conf")
+
+print(output)
